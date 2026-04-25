acd2bd253e505c2ae53b42e76901ed90ca5c2cf3
ovz <ovz@MacBook-Pro-ovz.local>
2026-04-26 01:24:05 +0400

    Refactor BottomNav with icons, extract NavButton, add PlusIcon

---
+++ src/components/Icons/PlusIcon.tsx
@@ -0,0 +1,6 @@
+export const PlusIcon = ({ size = 48 }: { size?: number }) => (
+  <svg width={size} height={size} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="24" cy="24" r="24" fill="white"/>
+    <path d="M24 14V34M14 24H34" stroke="black" strokeWidth="3" strokeLinecap="round"/>
+  </svg>
+);
